ed809895c19f4d9f3dfd9b7303e958a48e26495b
Muhammad Naufal <mnaufal0999@gmail.com>
2022-03-16 09:13:38 +0700

    initial commit

--- addons/registration_form/__init__.py
+++
@@ -1,5 +0,0 @@
-# First must be wizard and then models
-
-from . import wizards
-from . import models
-from . import reports
--- addons/registration_form/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import RegisUser
-from . import RegisClient
-from . import SaleOrder
-from . import ScheduleUser
-from . import UserTodo
-from . import UserContacts
